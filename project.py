ef0dc096dbaa51e87df9ab98f7def36a4fc5ce86
Andrew Glines <aglines@gmail.com>
2023-02-08 17:05:49 +0100

    tests all pass on DC, final commit

--- project.py
+++ project.py
@@ -1,49 +1,49 @@
-# Create a matplotlib scatter plot of the data that contains the following attributes:
-import matplotlib as plt
-import numpy as np
 import pandas as pd
-########################### DATA STRUCTURES PREP
-
-##initalize a matplotlib.pyplot fig object, which you can do using the code fig = plt.figure()
+from matplotlib import pyplot as plt
+import numpy as np
+office = pd.read_csv("datasets/office_episodes.csv")
+office.set_index("episode_number")
 fig = plt.figure()
-##A title, reading "Popularity, Quality, and Guest Appearances on the Office"
+
+most_views = 0.0
+for i in range(188):
+    x = office.loc[i, "viewership_mil"]
+    if x >= most_views:
+        most_views = x
+        top_star_mult = office.loc[i, "guest_stars"]
+
+top_star = ""
+for n in top_star_mult:
+    if n != ",":
+        top_star += n
+    if n == ",":
+        break
+
+colorlist = []
+for j in range(188):
+    scaled_ratings = office.loc[j, "scaled_ratings"]
+    curr_color = ""
+    if scaled_ratings < 0.25 : curr_color = "red"
+    if (scaled_ratings >= 0.25 and scaled_ratings < 0.50) : curr_color = "orange"
+    if (scaled_ratings >= 0.50 and scaled_ratings < 0.75) : curr_color = "lightgreen"
+    if scaled_ratings >= 0.75 : curr_color = "darkgreen"
+    colorlist.append(curr_color)
+
+sizes = []
+for j in range(188):
+    has_guests = office.loc[j, "has_guests"]
+    if has_guests == True : sizes.append(250)
+    if has_guests == False : sizes.append(25)
+
 plt.title("Popularity, Quality, and Guest Appearances on the Office")
-##An x-axis label reading "Episode Number"
 plt.xlabel("Episode Number")
-##A y-axis label reading "Viewership (Millions)"
 plt.ylabel("Viewership (Millions)")
 
-##Find  ONE of the guest stars (there were many) in MOST watched Office ep, Save it as  top_star
-# find most watched ep,
+xax = office.loc[:, "episode_number"]
+yax = office.loc[:, "viewership_mil"]
 
-max
-#   find names of guest stars in that ep, pick one randomly
-top_star = ""
+plt.rcParams['figure.figsize'] = [11, 7]
 
-##A color scheme reflecting the scaled ratings (not the regular ratings) of each episode, such that:
-##Ratings < 0.25 are colored "red" ,  Ratings >= 0.25 and < 0.50 are colored "orange"
-##Ratings >= 0.50 and < 0.75 are colored "lightgreen"  Ratings >= 0.75 are colored "darkgreen"
+plt.scatter(xax, yax, s = sizes, c = colorlist)
+plt.show(fig)
 
-# color list is a dict, so I could ...define it for each dtpt AS i genearte it OR ... ADD COLUMN to orig dt ,,,?
-
-colorlist = {}
-
-
-##Sizing system, such that episodes with guest appearances have a marker size of 250 and episodes without are sized 25
-
-#size argument is an nparray when given in the plot call, but .. how to seu it
-
-#         Each episode's episode number plotted along the x-axis
-#         Each episode's viewership (in millions) plotted along the y-axis
-ep_number = []
-ep_viewership = []
-
-
-###################### MAIN
-# read csv into a dataframe so i can see data
-office = pd.read_csv("datasets/office.csv")
-
-
-plt.scatter(ep_number, ep_viewership, c = colorlist)
-
-plt.show()
